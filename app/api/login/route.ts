5a1540c03d18a849974112d6af431997dc656f9c
Manish Chand <manishtochand@gmail.com>
2025-11-29 14:56:55 +0530

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
   const { username, password } = await request.json();
 
-  if (username === 'dark-manish' && password === 'Spiderman123') {
+  if (username === 'azureuser' && password === 'password') {
     return NextResponse.json({ message: 'Login successful' }, { status: 200 });
   } else {
     return NextResponse.json({ message: 'Invalid username or password' }, { status: 401 });
